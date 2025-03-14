44e4a297f30b9a38e0a8f5d4aa8eda44c0e77741
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-14 17:39:27 +0300

    vedrocraft-api update

--- src/main/java/ru/sema1ary/spawn/listener/DeathListener.java
+++ src/main/java/ru/sema1ary/spawn/listener/DeathListener.java
@@ -3,7 +3,7 @@ package ru.sema1ary.spawn.listener;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import ru.sema1ary.spawn.service.SpawnService;
 import ru.sema1ary.vedrocraftapi.service.ConfigService;
 
@@ -13,7 +13,7 @@ public class DeathListener implements Listener {
     private final ConfigService configService;
 
     @EventHandler
-    public void onDeath(PlayerDeathEvent event) {
+    public void onDeath(PlayerRespawnEvent event) {
         if(configService.get("teleport-when-player-death")) {
             spawnService.teleportToSpawn(event.getPlayer());
         }
